0fccbd1692cea182e07b0d9e13720145b23393b6
Lucas May Petry <lucasmaypetry@gmail.com>
2018-09-27 08:14:20 -0300

    Fixed news search within topic page

--- scrap_ndonline.py
+++ scrap_ndonline.py
@@ -2,6 +2,7 @@ import scrapy
 import psycopg2
 from scrapy.http import Request
 
+
 class NdOnlineSpider(scrapy.Spider):
     name = 'ndonline'
 
@@ -10,15 +11,13 @@ class NdOnlineSpider(scrapy.Spider):
     url_base = "https://ndonline.com.br"
 
     def parse(self, response):
-
         for title in response.css('.side-noticias li'):
             next_link = self.url_base + title.xpath('a/@href').extract_first()
 
             for page in range(2, 3):
                 yield Request(next_link + "?p=" + str(page), callback=self.parse_topics)
 
-    def parse_topics(self,response):
-    
+    def parse_topics(self, response):
         for news in response.css('.secoes-noticias .row'):
             topic = news.css('h3::text').extract_first()
             news_suffix = news.xpath('div/h4/a/@href').extract_first()
@@ -31,8 +30,7 @@ class NdOnlineSpider(scrapy.Spider):
             request.meta['topic'] = topic
             yield request
 
-    def parse_news(self,response):
-
+    def parse_news(self, response):
         title = response.css('.materia-header h1::text').extract()[0]
         subtitle = response.css('.materia-header p::text').extract()[0]
         date_time = response.css('.materia-header .materia-autor time::text').extract()[0]
