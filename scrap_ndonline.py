f994b798b24273389e15d8d2ace85744c9c6b747
Lucas May Petry <lucasmaypetry@gmail.com>
2018-09-27 08:13:39 -0300

    Fixed news search within topic page

--- scrap_ndonline.py
+++ scrap_ndonline.py
@@ -3,38 +3,41 @@ import psycopg2
 from scrapy.http import Request
 
 class NdOnlineSpider(scrapy.Spider):
-	name = 'ndonline'
+    name = 'ndonline'
 
-	start_urls = ['https://ndonline.com.br/florianopolis/noticias']
+    start_urls = ['https://ndonline.com.br/florianopolis/noticias']
 
-	url_base = "https://ndonline.com.br"
+    url_base = "https://ndonline.com.br"
 
-	def parse(self, response):
+    def parse(self, response):
 
-		for title in response.css('.side-noticias li'):
-			next_link = self.url_base + title.xpath('a/@href').extract_first()
+        for title in response.css('.side-noticias li'):
+            next_link = self.url_base + title.xpath('a/@href').extract_first()
 
-			for page in range(2, 3):
-				yield Request(next_link + "?p=" + str(page), callback=self.parse_topics)
+            for page in range(2, 3):
+                yield Request(next_link + "?p=" + str(page), callback=self.parse_topics)
 
-	def parse_topics(self,response):
-	
-		for news in response.css('.secoes-noticias .row'):
-			topic = news.css('h3::text').extract()
-			news_link = self.url_base + news.xpath('h4/a/@href').extract_first()
+    def parse_topics(self,response):
+    
+        for news in response.css('.secoes-noticias .row'):
+            topic = news.css('h3::text').extract_first()
+            news_suffix = news.xpath('div/h4/a/@href').extract_first()
 
-			request = Request(news_link, callback=self.parse_news)
-			request.meta['topic'] = topic
-			yield request
+            if not news_suffix:
+                continue
 
-	def parse_news(self,response):
+            news_link = self.url_base + news_suffix
+            request = Request(news_link, callback=self.parse_news)
+            request.meta['topic'] = topic
+            yield request
 
-		title = response.css('.materia-header h1::text').extract()[0]
-		subtitle = response.css('.materia-header p::text').extract()[0]
-		date_time = response.css('.materia-header .materia-autor time::text').extract()[0]
-		text = response.css('.materia-conteudo-container div div').extract()[0]
-		author = ""
-		portal = "NDONLINE"
-		tags = response.css('.materia-conteudo-container .tags a::text').extract()
-		#subject = 
-		print(response.meta['topic'])
+    def parse_news(self,response):
+
+        title = response.css('.materia-header h1::text').extract()[0]
+        subtitle = response.css('.materia-header p::text').extract()[0]
+        date_time = response.css('.materia-header .materia-autor time::text').extract()[0]
+        text = response.css('.materia-conteudo-container div div').extract()[0]
+        author = ""
+        portal = "NDONLINE"
+        tags = response.css('.materia-conteudo-container .tags a::text').extract()
+        subject = response.meta['topic']
