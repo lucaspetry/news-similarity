a9841fdc47f26b66bcd16af105d8a884d1d77d22
cahmilk <camila.leite002@gmail.com>
2018-09-28 16:06:31 -0300

    Update to insert news on the database

--- create_db.py
+++ create_db.py
@@ -12,7 +12,7 @@ cur = conn.cursor()
 cur.execute("""
 
 CREATE TABLE news(
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     title VARCHAR(200),
     subtitle VARCHAR(300),
     date_time TIMESTAMP WITHOUT TIME ZONE,
@@ -20,7 +20,8 @@ CREATE TABLE news(
     authors VARCHAR(200),
     tags VARCHAR(300),
     subject VARCHAR(100),
-    portal VARCHAR(100)
+    portal VARCHAR(100),
+    link VARCHAR(200)
 )
 """)
 conn.commit()
--- scrap_ndonline.py
+++ scrap_ndonline.py
@@ -1,14 +1,32 @@
+'''
+Created on Sep 26, 2018
+
+@authors: Camila Leite, Lucas May
+'''
 import scrapy
 import psycopg2
 from scrapy.http import Request
-
+from datetime import datetime
+import re
 
 class NdOnlineSpider(scrapy.Spider):
+
+    dbname = "news_articles"
+    dbhost = "localhost"
+    dbuser = "postgres"
+    dbpass = "postgres"
+
     name = 'ndonline'
 
     start_urls = ['https://ndonline.com.br/florianopolis/noticias']
 
     url_base = "https://ndonline.com.br"
+
+    def __init__(self):
+        self.conn = psycopg2.connect("dbname='" + self.dbname +
+                        "' user='" + self.dbuser +
+                        "' host='" + self.dbhost +
+                        "' password='" + self.dbpass + "'")
 
     def parse(self, response):
         for title in response.css('.side-noticias li'):
@@ -31,11 +49,43 @@ class NdOnlineSpider(scrapy.Spider):
             yield request
 
     def parse_news(self, response):
-        title = response.css('.materia-header h1::text').extract()[0]
-        subtitle = response.css('.materia-header p::text').extract()[0]
-        date_time = response.css('.materia-header .materia-autor time::text').extract()[0]
-        text = response.css('.materia-conteudo-container div div').extract()[0]
+        
+        tag_re = re.compile(r"<[^>]+>")
+
+        title = response.css('.materia-header h1::text').extract()[0].replace("'", "")
+
+        subtitle = response.css('.materia-header p::text').extract()[0].replace("'", "")
+
+        date_time = response.css('.materia-header .materia-autor time::text').extract()[0].replace("h", ':')
+        date_time = datetime.strptime(date_time, '%d/%m/%Y %H:%M')
+
+        text = response.css('.materia-conteudo-container div div').extract()[0].replace("'", "")
+        text = tag_re.sub("", text)
+
         author = ""
+
         portal = "NDONLINE"
+
         tags = response.css('.materia-conteudo-container .tags a::text').extract()
+
         subject = response.meta['topic']
+
+        link = response.url
+
+        cur = self.conn.cursor()
+         
+        cur.execute("select count(*) from news where title = '" + title + "' AND subtitle = '" + subtitle + "' AND portal = '" + portal + "'")
+        if cur.fetchall()[0][0] > 0:
+            return
+
+        query = "insert into news (title, subtitle, date_time, text, authors, portal, tags, subject, link) " + \
+                "values ('" + title + "', '" + subtitle + "', '" + str(date_time) + "', '" + text + "', '" + author + "', '" + \
+                portal + "', '" + str(tags).replace("'", '') + "', '" + subject + "', '" + link + "')" 
+
+        try:
+            cur.execute(query)
+            self.conn.commit()
+        except e:
+            print("\n\n\nQuery Error: " + str(e) + "\n\n\n\n")
+            self.conn.rollback()
+
