164218616a10db9a5a02f4b11df030126e58eaa3
cahmilk <camila.leite002@gmail.com>
2018-10-18 15:47:01 -0300

    Update text extract wip

--- scrap_nsc_total.py
+++ scrap_nsc_total.py
@@ -29,11 +29,13 @@ class NSCTotal(scrapy.Spider):
                         "' password='" + self.dbpass + "'")
 
     def parse(self, response):
-        for title in response.css('div .iJUApm li'):
+        i = 0
+        for title in response.xpath("//*[contains(@class, 'iJUApm')]//li"):
             next_link = title.xpath('a/@href').extract()
-            print(next_link)
-            if next_link[0] != '/':
+            if i > 0:
                 yield Request(next_link[0], callback=self.parse_topics)
+                return
+            i += 1
 
     def parse_topics(self,response):
         news_suffix = response.css('h1::text').extract_first()
@@ -42,21 +44,22 @@ class NSCTotal(scrapy.Spider):
             request = Request(news_link, callback=self.parse_news)
             request.meta['topic'] = news_suffix
             yield request
+            return
 
     def parse_news(self, response):
-        
+        text_re = re.compile(r"<[^>]+>")
         date_re = re.compile(r"(Atualizada em: [0-9/]+ [0-9:]+)")
-        
         title = response.css('h1::text').extract_first()
-
         subtitle = ""
-
+        
         date_time = response.css('div .DateInfo ::text').extract_first().replace("min", '').replace("h", ':').replace("- ", '')
         date_time = date_re.sub("", date_time)
         date_time = datetime.strptime(date_time, '%d/%m/%Y %H:%M')
 
-        text = response.css('div .entry-content ::text').extract_first().replace("'", "")
+        get_full_text = response.xpath("//*[contains(@class, 'StyledParagraph')]").extract()
+        print(get_full_text)
+        text = ""
+        for p in get_full_text:
+            print(p)
 
-
-        return
 
