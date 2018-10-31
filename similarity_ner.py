c5658d3b45e594fbb42d07506774294aab35b453
Lucas May Petry <lucasmaypetry@gmail.com>
2018-10-30 21:19:21 -0300

    Sample code NER with Spacy

--- similarity_ner.py
+++ similarity_ner.py
@@ -1,5 +1,20 @@
-from nltk.tokenize import word_tokenize
+import spacy
 
-#
-# TODO - Extract NERs and compare news
-#
+
+nlp = spacy.load('pt_core_news_sm')
+
+# Process whole documents
+text = (u"O Ministério Público de Santa Catarina (MPSC) entrou"
+        u" na Justiça com ação civil pública nesta terça-feira"
+        u" (30) contra a deputada estadual eleita Ana Caroline"
+        u" Campagnolo (PSL). O órgão quer a condenação por danos"
+        u" morais coletivos e pede que seja dada liminar (decisão"
+        u" temporária) para que ela se abstenha de manter qualquer"
+        u" tipo de controle ideológico das atividades dos"
+        u" professores e alunos de escolas públicas e privadas"
+        u" do estado.")
+doc = nlp(text)
+
+# Find named entities, phrases and concepts
+for entity in doc.ents:
+    print(entity.text, entity.label_)
