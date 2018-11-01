ab00ec3ef679cc4fecc5180d61e8b5b717a5887e
cahmilk <camila.leite002@gmail.com>
2018-11-01 13:53:15 -0300

    Bench manipulation wip[

--- news_loader.py
+++ news_loader.py
@@ -1,23 +1,26 @@
 import psycopg2
 
 dbname = "news_articles"
-dbhost = "localhost"
+dbhost = "150.162.58.58"
 dbuser = "postgres"
-dbpass = "postgres"
+dbpass = "Trajetorias123"
 
 
-def load_news(fields=['id', 'title', 'text', 'portal']):
-    conn = psycopg2.connect("dbname='" + dbname +
+def connect():
+    return psycopg2.connect("dbname='" + dbname +
                             "' user='" + dbuser +
                             "' host='" + dbhost +
                             "' password='" + dbpass + "'")
 
+def load_news(fields=['id', 'title', 'text', 'portal']):
+    
+    conn = connect()
     cur = conn.cursor()
     field_names = str(fields).replace("'", "") \
                              .replace("[", "") \
                              .replace("]", "")
 
-    query = "SELECT " + field_names + " FROM news WHERE date_time <= '22/10/2018' AND date_time >= '22/09/2018'"
+    query = "SELECT " + field_names + " FROM news WHERE date_time <= '22/10/2018' AND date_time >= '22/09/2018' ORDER BY id ASC"
     news = []
 
     cur.execute(query)
@@ -26,3 +29,18 @@ def load_news(fields=['id', 'title', 'text', 'portal']):
         news.append(data)
 
     return news
+
+def load_distinct(field_name):
+    
+    conn = connect()
+    cur = conn.cursor()
+
+    query = "SELECT DISTINCT" + field_name + "FROM news WHERE date_time <= '22/10/2018' AND date_time >= '22/09/2018'"
+    
+    cur.execute(query)
+
+    fields = []
+    for field in cur.fetchall():
+        fields.append(field[0])
+
+    return fields
