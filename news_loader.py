0ff9441ce1ca575b69e54ec488d34235e3fc7341
cahmilk <camila.leite002@gmail.com>
2018-11-01 14:27:00 -0300

    Minha mensagem wip

--- news_loader.py
+++ news_loader.py
@@ -35,7 +35,7 @@ def load_distinct(field_name):
     conn = connect()
     cur = conn.cursor()
 
-    query = "SELECT DISTINCT" + field_name + "FROM news WHERE date_time <= '22/10/2018' AND date_time >= '22/09/2018'"
+    query = "SELECT DISTINCT " + field_name + " FROM news WHERE date_time <= '22/10/2018' AND date_time >= '22/09/2018'"
     
     cur.execute(query)
 
@@ -44,3 +44,34 @@ def load_distinct(field_name):
         fields.append(field[0])
 
     return fields
+
+def add_score(ids, field, score):
+    conn = connect()
+    cur = conn.cursor()
+
+    query = "SELECT COUNT(*) FROM similarity WHERE news_id1 = :id1 AND news_id2 = :id2"
+    query = query.replace(":id1", str(ids[0]))
+    query = query.replace(":id2", str(ids[1]))
+    cur.execute(query)
+
+    def get_insert():
+        insert = "INSERT INTO similarity (news_id1, news_id2, " + field + ") VALUES (:id1, :id2, :score)"
+        insert = insert.replace(":id1", str(ids[0]))
+        insert = insert.replace(":id2", str(ids[1]))
+        insert = insert.replace(":score", str(score))
+        return insert
+
+    def get_update():
+        update = "UPDATE similarity WHERE news_id1 = :id1, news_id2 = :id2 SET " + field + " = :score"
+        update = update.replace(":id1", str(ids[0]))
+        update = update.replace(":id2", str(ids[1]))
+        update = update.replace(":score", str(score))
+        return update
+
+    if cur.fetchall()[0][0] > 0:
+        surprise_query = get_update()
+    else:
+        surprise_query = get_insert()
+
+    cur.execute(surprise_query)
+    conn.commit()
